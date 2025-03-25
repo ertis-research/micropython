10d19d78b4c6f4b2f45325ea8d7f1c48d4b8aae6
antoniochavesgarcia <chaves@uma.es>
2025-03-25 09:37:45 +0100

    feat: Added sdcard extmod.

--- ports/stm32/boards/manifest.py
+++ ports/stm32/boards/manifest.py
@@ -1,6 +1,7 @@
 include("$(MPY_DIR)/extmod/asyncio")
 include("$(MPY_DIR)/extmod/notecard")
 include("$(MPY_DIR)/extmod/umodbus")
+include("$(MPY_DIR)/extmod/sdcard")
 
 require("dht")
 require("onewire")
