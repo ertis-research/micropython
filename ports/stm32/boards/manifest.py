cab129fc1e23547d7a79314de00b4b859036f514
antoniochavesgarcia <chaves@uma.es>
2025-03-25 11:54:55 +0100

    fix: included sd card as py lib. ideal to learn how to read.

--- ports/stm32/boards/manifest.py
+++ ports/stm32/boards/manifest.py
@@ -1,7 +1,7 @@
 include("$(MPY_DIR)/extmod/asyncio")
 include("$(MPY_DIR)/extmod/notecard")
 include("$(MPY_DIR)/extmod/umodbus")
-# include("$(MPY_DIR)/extmod/sdcard") # There is an C module for SD card
+include("$(MPY_DIR)/extmod/sdcard") # There is an C module for SD card
 
 require("dht")
 require("onewire")
